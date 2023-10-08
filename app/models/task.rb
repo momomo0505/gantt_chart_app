02be55615801c7ec0dddada6c5b4f6325a6f8f4c
momomo0505 <techcamp.fujita.t@gmail.com>
2023-10-08 17:27:57 +0900

    エラー表示の修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,8 +3,17 @@ class Task < ApplicationRecord
   validates :blockname, length: { maximum: 50 }, allow_blank: true
   validates :blockname2, length: { maximum: 50 }, allow_blank: true
   validates :blockname3, length: { maximum: 50 }, allow_blank: true
+  validate :end_date_after_start_date
 
   def is_active_on?(date)
     start_date <= date && date <= end_date
+  end
+
+  private
+
+  def end_date_after_start_date
+    if end_date && start_date && end_date < start_date
+      errors.add(:end_date, "は開始日より後の日付を選択してください")
+    end
   end
 end
